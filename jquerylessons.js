a5eca4e76b9574a77b151fbac79be9cd11ca6dac
= <tsmit291@icloud.com>
2015-12-02 15:12:24 -0700

    chained functions to ajax call function

--- jquerylessons.js
+++ jquerylessons.js
@@ -3,14 +3,11 @@ var namey = "";
 $("#button").click(function(){
     namey = $("#searchBox").val();
     $("tr").not(".firstClass").remove();
-    var getter = $.ajax({
+    $.ajax({
       url: "https://www.reddit.com/subreddits/search.json?q="+namey,
       method: "GET",
       dataType: "json"
-    });
-      // calling to the url with the get method and getting json type of data back. in this case we are looking for banana.
-
-    getter.done(function(response){
+    }).done(function(response){
       var arr = response["data"]["children"]
       for (var i = 0; i < arr.length; i++) {
         var inside = (arr[i]["data"]["title"]);
