63440656207cc189bb672ead48df751b28e80625
= <tsmit291@icloud.com>
2015-12-02 16:41:08 -0700

    refactoring part 1

--- jquerylessons.js
+++ jquerylessons.js
@@ -8,18 +8,12 @@ $("#button").click(function(){
       method: "GET",
       dataType: "json"
     }).done(function(response){
-      var arr = response["data"]["children"]
-      for (var i = 0; i < arr.length; i++) {
-        var inside = (arr[i]["data"]["title"]);
-        var description = arr[i]["data"]["public_description"];
-        var image = arr[i]["data"]["header_img"] ? arr[i]["data"]["header_img"] : "http://placehold.it/40x40";
-        $(".firstClass").after("<tr><td>"+ inside +"</td>"+"<td>"+ description +"</td>"+"<td>" + "<img src=" + image + ">" + "</td></tr>");
-      }
-    });
+      print(response)
+
+    })
     // get something reddit and then come back. when you're done, hand it over if successful.
     // response is the object itself that you are getting back from the server.
     // getter.fail(function(){
-    //   console.log("those people are mean");
     // });
 
     $("#removeButton").click(function(){
@@ -30,3 +24,15 @@ $("#button").click(function(){
 
     })
 });
+
+ 
+
+ function print(responsible){
+      var arr = responsible["data"]["children"]
+      var items = responsible["inside","description","image"]
+      for (var i = 0; i < arr.length; i++) {
+        var inside = (arr[i]["data"]["title"]);
+        var description = arr[i]["data"]["public_description"];
+        var image = arr[i]["data"]["header_img"] ? arr[i]["data"]["header_img"] : "http://placehold.it/40x40";
+        $(".firstClass").after("<tr><td>"+ inside +"</td>"+"<td>"+ description +"</td>"+"<td>" + "<img src=" + image + ">" + "</td></tr>");
+}}
